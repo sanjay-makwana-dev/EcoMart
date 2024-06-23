9a1760fbfed22b9690f4c9c4d74339705d229392
sanjay makwana <sasanjaymakwana@gmail.com>
2024-06-23 13:18:50 +0530

    Add project files.

---
+++ EcoMart.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcoMart.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
